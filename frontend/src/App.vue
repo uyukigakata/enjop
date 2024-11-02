b116924e8555b596cd4c0703a18b415d4c2aebed
tomo0611 <tomo0611@hotmail.com>
2024-11-02 22:47:02 +0900

    [add] メインページのデザイン完成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,100 +1,34 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <header>
-    <v-toolbar color="primary" dark="true">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
+    <v-toolbar color="#FFF" dark="true">
+      <v-toolbar-title class="top-logo">SecHack 365</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
+        <v-icon>mdi-account-circle</v-icon>
       </v-btn>
     </v-toolbar>
-
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <v-toolbar color="primary" dark="true">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>炎上チェッカー</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
   </header>
 
   <RouterView />
+  <footer>
+    <Footer></Footer>
+  </footer>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.top-logo {
+  margin-left: 72px;
+  height: fit-content;
 }
 </style>
