9a0c8665aef69395e521b7bc4c9b5c26aff6bf40
tomo0611 <tomo0611@hotmail.com>
2024-11-02 22:06:03 +0900

    [add] Add mdi/font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,12 +9,16 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 const app = createApp(App)
 
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
 })
 
 app.use(router)
